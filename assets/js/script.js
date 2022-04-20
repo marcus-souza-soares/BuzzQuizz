15cc02368d16ea915a94bde6832bd372affece0f
Marcus <marcusadrenaline@gmail.com>
2022-04-20 18:25:41 -0300

    inicio do server

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+function renderizarQuizzesServer(){
+
+
+    const container = document.querySelector(".quizzes");
+    const quizzes = (obj) => {
+        const dados = obj.data;
+
+
+        container.innerHTML += `<div class="quizz">
+                                    <img src=${dados.image}>
+                                    <h2>${dados.title}</h2>
+                                </div>`
+
+    } 
+
+    const promiseGet = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/2');
+    promiseGet.then(quizzes);
+}
+renderizarQuizzesServer();
