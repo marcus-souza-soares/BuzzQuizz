6bf22e55d1b6d2288c0d6d0fad549b10e61b1fb2
Marcus <marcusadrenaline@gmail.com>
2022-04-21 10:17:53 -0300

    terminada primeira tarefa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 
+//Variavies globais
+let id_quizz;
+
+
+
+
 
 function renderizarQuizzesServer(){
 
@@ -8,7 +14,7 @@ function renderizarQuizzesServer(){
         const dados = obj.data;
 
         for (i = 0; i < dados.length; i++){
-            container.innerHTML += `<div class="quizz">
+            container.innerHTML += `<div class="quizz" onclick="paginaQuizz()">
                                         <img src=${dados[i].image}>
                                         <div class="titulo">
                                             <h2>${dados[i].title}</h2>
@@ -24,3 +30,28 @@ function renderizarQuizzesServer(){
 }
 renderizarQuizzesServer();
 
+
+
+
+// Parte do código de abrir um quiz
+
+function paginaQuizz () {
+
+    //@LIMPAR A PÁGINA@
+    const container = document.querySelector(".principal");
+    container.innerHTML = ""
+
+    const quizzes = (obj) => {
+        const dados = obj.data;
+        //apenas para ver se ta funcionando, o codigo ta sendo construido do teste.js
+            console.log("123456")
+       
+    }
+        
+        
+
+     // COLOQUEI A VERSÃO DA TURMA 4 APENAS PARA VER COMO FICARIA
+    const promiseGet = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+    promiseGet.then(quizzes);
+
+}
