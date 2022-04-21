208d5c9957134c546623c8b561f184c8eac94f7f
Marcus <marcusadrenaline@gmail.com>
2022-04-21 00:30:43 -0300

    redenrização dos quizzes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,27 +7,20 @@ function renderizarQuizzesServer(){
     const quizzes = (obj) => {
         const dados = obj.data;
 
+        for (i = 0; i < dados.length; i++){
+            container.innerHTML += `<div class="quizz">
+                                        <img src=${dados[i].image}>
+                                        <div class="titulo">
+                                            <h2>${dados[i].title}</h2>
+                                        </div>
+                                    </div>`
+        }
+        
+        
 
-        container.innerHTML += `<div class="quizz">
-                                    <img src=${dados.image}>
-                                    <h2>${dados.title}</h2>
-                                </div>`
-
-    } 
-    const promiseGet = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/2');
+    } // COLOQUEI A VERSÃO DA TURMA 4 APENAS PARA VER COMO FICARIA
+    const promiseGet = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
     promiseGet.then(quizzes);
 }
 renderizarQuizzesServer();
 
-
-
-
-console.log("teste de git");
-//const container = lista_idquizz.map(renderizarQuizzesServer);
-//container();
-
-function aaaa (teste) {
-
-    console.log('oiiii');
-
-}
